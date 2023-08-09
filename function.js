91b4bb5cbd04ff972bc7f0d2fb182dfea44aad8c
Neethusanal <neethukumarkk@gmail.com>
2023-08-09 13:24:34 +0530

    "basics"

---
+++ function.js
@@ -0,0 +1,22 @@
+// console.log(sum)
+// function sum(a,b,c)
+// {
+//     return(a+b+c)
+
+// }
+console.log(sum(),"function expression")
+var sum=function(a,b,c)
+{
+    return a+b+c
+}
+console.log(sum(2,3,4))//Here it will show error due to hoisting
+
+
+
+
+// function sum(a,b,c)
+// {
+    
+// console.log((a+b+c))
+// }
+// console.log(sum(2,3,4))  //since there is no return it will be undefined
---
+++ push.js
@@ -0,0 +1,10 @@
+var numbers=[]
+numbers.push(10)
+numbers.push(20)
+numbers.push(30)
+console.log(numbers)
+console.log(numbers.push(40)) //push function returns the number of elements in array
+console.log(numbers)
+numbers.pop()
+console.log(numbers)
+console.log(numbers.pop()) // pop function return the element that is poped put of array
