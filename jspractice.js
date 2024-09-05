399c6b5759a6a899ff6f50bbe0577141e170772b
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2024-09-05 09:10:02 +0300

    hoisting

--- jspractice.js
+++ jspractice.js
@@ -17,11 +17,31 @@ console.log(x)//due to hoisting behaviour of var
 }
 
 // ******************************************************************************************************
-if([])
-    {
-        console.log("hello") //the reason it prints "hello" is that an empty array ([]) is not a falsy value in JavaScript. In fact, it's a truthy value.
-    }
-    else
-    {
-        console.log("bye")
-    }
+// if([])
+//     {
+//         console.log("hello") //the reason it prints "hello" is that an empty array ([]) is not a falsy value in JavaScript. In fact, it's a truthy value.
+//     }
+//     else
+//     {
+//         console.log("bye")
+//     }
+// *********************************************************************
+
+ console.log(NaN!=NaN)
+ 
+ console.log(1!==true)
+ console.log(1!=true)
+
+ //**************************************************************************** */
+
+ greet("john")
+
+ var greet= (name)=>{
+    console.log("good day", name)
+ }
+
+ function greet (name)
+{
+    console.log("Hello ", name)
+}
+greet("john")
